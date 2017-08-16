1178c096abb0618f26f4235a94a904911ba61820
Sungpil Han CPT <shan@acp.kr>
2017-08-16 18:22:01 +0900

    noncomp 0.3.3

--- R/noncompart_0.3.3.R
+++ R/noncompart_0.3.3.R
@@ -1,8 +1,11 @@
 # setup ----
 
 library(tidyverse)
-library(NonCompart, lib = './lib')
+library(NonCompart, lib = './lib') # 0.3.3
+library(ncar)
+
 # install.packages("lib/NonCompart_0.3.3.tar.gz", repos = NULL, lib = "./lib", type = "source")
+# install.packages("lib/NonCompart_0.3.3.tar.gz", repos = NULL, type = "source")
 
 tblNcaWrap <- function(df) {
   parameters <- attributes(df)$dimnames[[2]]
@@ -22,4 +25,6 @@ tblNcaWrap <- function(df) {
 Conc <- read.csv("lib/NonCompart-src/tests/BE-Conc.csv", as.is=TRUE) # folder 위치는 맞추세요.
 Res1 <- tblNCA(Conc, key=c("SUBJ", "GRP", "PRD", "TRT"), colTime="TIME", colConc="CONC", dose=100)
 
-tblNcaWrap(Res1) %>% View
+tblNcaWrap(Res1) #%>% View
+
+
